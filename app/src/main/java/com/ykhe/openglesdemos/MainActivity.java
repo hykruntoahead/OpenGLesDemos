dfb66de426848534440ea9bcb0bcca0f5d2d9a67
ykhe <ykhe@grandstream.cn>
2020-11-13 14:42:47 +0800

    绘制三角形 Triangle

--- app/src/main/java/com/ykhe/openglesdemos/MainActivity.java
+++ app/src/main/java/com/ykhe/openglesdemos/MainActivity.java
@@ -2,6 +2,7 @@ package com.ykhe.openglesdemos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +10,7 @@ import android.view.View;
 import com.ykhe.openglesdemos.helloword.HelloWorld;
 import com.ykhe.openglesdemos.line.DrawLine;
 import com.ykhe.openglesdemos.point.DrawPoint;
+import com.ykhe.openglesdemos.triangle.DrawTriangle;
 
 /**
  * https://wiki.jikexueyuan.com/project/opengl-es-guide/
@@ -22,8 +24,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.btn_to_hw).setOnClickListener(this);
         findViewById(R.id.btn_to_dp).setOnClickListener(this);
         findViewById(R.id.btn_to_dl).setOnClickListener(this);
+        findViewById(R.id.btn_to_dt).setOnClickListener(this);
     }
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     public void onClick(View v) {
         switch (v.getId()){
@@ -35,6 +39,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.btn_to_dl:
                 startActivity(new Intent(this, DrawLine.class));
+                break;
+            case R.id.btn_to_dt:
+                startActivity(new Intent(this, DrawTriangle.class));
+            default:
                 break;
         }
     }
